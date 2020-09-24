1f0d0478accca1bf5750a7202bb6cd6ac376d4c9
meraj rashidi <67751068+merajrashidi69@users.noreply.github.com>
2020-09-24 21:20:04 +1000

    Update renamer_numbers.py

--- renamer_numbers.py
+++ renamer_numbers.py
@@ -4,15 +4,20 @@ def main():
     start = int(input('Folder numbers start from : '))
     number = 0
     number += start
-    print('Starting from',number)
-
+    print(f'Starting from {number}')
+    
     path = input('Path : ')
-    print('Changed path to',path)
+    print(f'Changed path to {path}')
     os.chdir(path)
+    
+    file_format = input('Enter the file format : ')
+    form = f'.{file_format}'
+    print(f'File format has been set to {file_format}.')
+    
     time.sleep(1.5)
     for x in os.listdir():
         number += 1
-        os.rename(x,str(number) + '.mp4')
+        os.rename(x,str(number) + form)
         print(x)
 
 while True:
